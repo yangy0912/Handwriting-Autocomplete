596e40b3b26c125c834c1c6a9a31c5c8ab81e5f8
Yang <Theoneandonlyyang@gmail.com>
2025-06-30 15:20:09 -0700

    redesigned matrix for weights, etc

--- MultilayerPerceptron/Classification.py
+++ MultilayerPerceptron/Classification.py
@@ -1,13 +1,32 @@
 import numpy as np
 import pandas as pd
+from DeepLearning import *
 
-def initial_data_shuffle_split():
-    df = pd.read_csv('../A_Z Handwritten Data/A_Z Handwritten Data.csv')
-    shuffled_df = df.sample(frac=1).reset_index(drop=True)
-    split_portion = int(len(df) * 0.8)
-    train_df = shuffled_df.iloc[:split_portion]
-    test_df = shuffled_df.iloc[split_portion:]
-    train_df.to_csv('../A_Z Handwritten Data/train_df_shuffled.csv')
-    test_df.to_csv('../A_Z Handwritten Data/test_df_shuffled.csv')
+weights = np.load('./Model/weights.npz')
+w1 = weights['w1']
+w2 = weights['w2']
+w3 = weights['w3']
+w4 = weights['w4']
+op = weights['op']
+
+def classify(pixels):
+    # supress all pixels to [0, 1]
+    pixels = pixels / 255.0
+    # zero out neurons
+    l1 = np.matmul(pixels, w1)
+    l2 = np.matmul(l1, w2)
+    l3 = np.matmul(l2, w3)
+    l4 = np.matmul(l3, w4)
+    output = np.matmul(l4, op)
+    # Softmax?
+    return output
+
+df = pd.read_csv('../A_Z Handwritten Data/train_df_shuffled.csv')
+row = df.iloc[0]
+label = row["0"]
+pixels = row.iloc[1:].to_numpy()
+print("label: " + str(label))
+print(len(row))
+
 
 
--- MultilayerPerceptron/DeepLearning.py
+++ MultilayerPerceptron/DeepLearning.py
@@ -11,19 +11,29 @@ learning_rate: float = 0.01
 bias: list[int] = [64, 64, 32, 32]
 
 # Neuron Container
-first_layer_neurons= np.zeros(hidden_layer_size[0])
-second_layer_neurons = np.zeros(hidden_layer_size[1])
-third_layer_neurons = np.zeros(hidden_layer_size[2])
-fourth_layer_neurons = np.zeros(hidden_layer_size[3])
+first_layer_neurons= np.zeros((hidden_layer_size[0], 1))
+second_layer_neurons = np.zeros((hidden_layer_size[1], 1))
+third_layer_neurons = np.zeros((hidden_layer_size[2], 1))
+fourth_layer_neurons = np.zeros((hidden_layer_size[3], 1))
+
+output_neurons = np.zeros((output_size, 1))
 
 def initialize_weights():
     scale = 1
-    initial_weight1 = np.random.rand(hidden_layer_size[0]) * scale
-    initial_weight2 = np.random.rand(hidden_layer_size[1]) * scale
-    initial_weight3 = np.random.rand(hidden_layer_size[2]) * scale
-    initial_weight4 = np.random.rand(hidden_layer_size[3]) * scale
+    initial_weight1 = np.random.rand(784, hidden_layer_size[0]) * scale
+    initial_weight2 = np.random.rand(64, hidden_layer_size[1]) * scale
+    initial_weight3 = np.random.rand(64, hidden_layer_size[2]) * scale
+    initial_weight4 = np.random.rand(32, hidden_layer_size[3]) * scale
+    initial_output_weight = np.random.rand(32, 26) * scale
     np.savez('./Model/weights.npz', w1=initial_weight1,
-             w2=initial_weight2, w3=initial_weight3, w4=initial_weight4)
+             w2=initial_weight2, w3=initial_weight3,
+             w4=initial_weight4, op=initial_output_weight)
 
+'''
 weights = np.load('./Model/weights.npz')
-print(weights['w1'])
+print(weights['w1'].shape)
+print(weights['w2'].shape)
+print(weights['w3'].shape)
+print(weights['w4'].shape)
+print(weights['op'].shape)
+'''
